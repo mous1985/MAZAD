f24e91c229612c56d96f137b74bd4c009717266d
mous1985 <mustapha.benazzouz@outlook.fr>
2024-10-09 10:24:17 +0200

    chore : use json package

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,9 +1,8 @@
-// src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App.tsx';
-import theme from './theme/theme'; // Import the custom theme
+import theme from './theme/theme';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
